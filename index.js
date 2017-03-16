39722ff4a4e1b7d84fd107d9975eb4cc11aaeb5d
System <none>
2017-03-16 12:58:02 +0000

    Checkpoint

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 var fs = require('fs'),
     path = require('path'),
     Twit = require('twit'),
-    config = require(path.join(__dirname, 'config.js'));
+    config = {
+    /* Be sure to update the .env file with your API keys. See how to get them: https://botwiki.org/tutorials/make-an-image-posting-twitter-bot/#creating-a-twitter-app*/
+      twitter: {
+        consumer_key: process.env.CONSUMER_KEY,
+        consumer_secret: process.env.CONSUMER_SECRET,
+        access_token: process.env.ACCESS_TOKEN,
+        access_token_secret: process.env.ACCESS_TOKEN_SECRET
+      }
+    };
 
 var T = new Twit(config);
 
