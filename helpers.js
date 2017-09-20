e94dd6b2573380ec3eecbcae47a9e497c3994135
System <none>
2017-09-20 14:21:01 +0000

    Exporting to fourtonfish/random-image-twitterbot-glitch

--- helpers.js
+++ helpers.js
@@ -9,6 +9,17 @@ module.exports = {
   get_random_int: function(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
   },
+  get_random_range: function(min, max, fixed) {
+    return (Math.random() * (max - min) + min).toFixed(fixed) * 1;
+  },
+  get_random_hex: function() {
+    return '#' + Math.random().toString(16).slice(2, 8).toUpperCase();
+  },
+  shade_color: function(color, percent) {
+    // https://stackoverflow.com/questions/5560248/programmatically-lighten-or-darken-a-hex-color-or-rgb-and-blend-colors
+    var f = parseInt(color.slice(1),16),t=percent<0?0:255,p=percent<0?percent*-1:percent,R=f>>16,G=f>>8&0x00FF,B=f&0x0000FF;
+    return `#${(0x1000000+(Math.round((t-R)*p)+R)*0x10000+(Math.round((t-G)*p)+G)*0x100+(Math.round((t-B)*p)+B)).toString(16).slice(1)}`;
+  },  
   load_image_assets: function(cb){
     /* Load images from the assets folder */
     console.log('reading assets folder...')
@@ -63,5 +74,10 @@ module.exports = {
           cb(err);
         }
     });
-  },  
+  },
+  download_file: function(uri, filename, cb){
+    request.head(uri, function(err, res, body){
+      request(uri).pipe(fs.createWriteStream(filename)).on('close', cb);
+    });
+  }
 };
